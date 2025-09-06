230fb96e4eb0298938dc51bfa5a25f70605d76e9
mauricio jaramillo <mauriciojaramillo1108@gmail.com>
2025-09-06 16:59:58 -0500

    se corrigen errores  de Userport
    se corrigen errores  de Userport

--- proyecto-integrador/src/main/java/app/domain/ports/UserPort.java
+++ proyecto-integrador/src/main/java/app/domain/ports/UserPort.java
@@ -4,7 +4,11 @@ import app.domain.model.MedicalInsurance;
 import app.domain.model.User;
 
 public interface UserPort {
-    public MedicalInsurance findByMedicalInsurance(MedicalInsurance medicalInsurance) throws Exception;
+    public UserPort userPort = null;
+	public User doctor = null;
+	public MedicalInsurance findByMedicalInsurance(MedicalInsurance medicalInsurance) throws Exception;
 	public User findByUserName(User user) throws Exception;
 	public void save(User user) throws Exception;
+	public User user = userPort.findByDocument(doctor);
+	public User findByDocument(User doctor);
 }
